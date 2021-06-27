b0f316670356d680311f12cb8ddd851f33dd711a
GustavTheUseless <gusw.petersen@gmail.com>
2021-06-28 01:27:54 +0200

    Initial Commit

---
+++ PartyPod/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PartyPod
+//
+//  Created by Gustav Walker Petersen on 28/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PartyPod/PartyPodApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyPodApp.swift
+//  PartyPod
+//
+//  Created by Gustav Walker Petersen on 28/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct PartyPodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
